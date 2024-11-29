c0e1fb3cfe7803d91ed198b6e2b05146e69ba78e
Bos8086 <Boma.obomanu86@gmail.com>
2024-11-29 16:47:14 +0000

    Started on the project, created the header banner

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import "./styles/App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "font-awesome/css/font-awesome.min.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className=" App-header-banner">
+          <i
+            aria-hidden="true"
+            className="fa fa-phone"
+            style={{ color: "#FF8A1E", fontSize: "25px" ,margin: "0 10px" }}
+          ></i>
+          <p className="App-header-text"> Call : +01 123455678990</p>
+        </div>
+        <div className="App-header-banner" aria-hidden="true">
+          <i
+            class="fa fa-envelope"
+            style={{ color: "#FF8A1E", fontSize: "25px" ,margin: "0 10px" }}
+          ></i>
+          <p className="App-header-text"> Email : demo@gmail.com</p>
+        </div>
       </header>
+      <footer className="App-footer">
+        <p className="App-footer-text">Get in Touch</p>
+      </footer>
     </div>
   );
 }
