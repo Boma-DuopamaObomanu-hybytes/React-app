5f25c071d94ffd1275d56b167e65d588edc851eb
Bos8086 <Boma.obomanu86@gmail.com>
2024-11-29 18:32:11 +0000

    Add the second part of the header and have started working on the navigation system

--- src/App.js
+++ src/App.js
@@ -9,19 +9,34 @@ function App() {
         <div className=" App-header-banner">
           <i
             aria-hidden="true"
-            className="fa fa-phone"
-            style={{ color: "#FF8A1E", fontSize: "25px" ,margin: "0 10px" }}
+            class="fa fa-phone"
+            style={{ color: "#FF8A1E", fontSize: "25px", margin: "0 10px" }}
           ></i>
           <p className="App-header-text"> Call : +01 123455678990</p>
         </div>
         <div className="App-header-banner" aria-hidden="true">
           <i
             class="fa fa-envelope"
-            style={{ color: "#FF8A1E", fontSize: "25px" ,margin: "0 10px" }}
+            style={{ color: "#FF8A1E", fontSize: "25px", margin: "0 10px" }}
           ></i>
           <p className="App-header-text"> Email : demo@gmail.com</p>
         </div>
       </header>
+
+      <div className="Navbar">
+        <div>
+          <p className="Navbar-text">INANCE</p>
+        </div>
+        <div className="Navbar-sidebar">
+          <ul className="Navbar-list">
+            <li>Home</li>
+            <li>About</li>
+            <li>Services</li>
+            <li>Contact Us</li>
+          </ul>
+        </div>
+      </div>
+
       <footer className="App-footer">
         <p className="App-footer-text">Get in Touch</p>
       </footer>
